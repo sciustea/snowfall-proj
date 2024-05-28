2fa1441ee194ca0dc5bc07c656aea067c5b9e45c
Stefana Ciustea <stefanaciustea@gmail.com>
2024-05-27 17:42:38 -0700

    sixth commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,17 @@
 
 import React, { useState } from 'react';
 import './App.css';
-import Timer from './components/Timer';
-import Settings from './components/Settings';
-import Sound from './components/Sound';
+import logo from './logo.jpg';
+
 
 function App() {
-  const [workDuration, setWorkDuration] = useState(25);
-  const [breakDuration, setBreakDuration] = useState(5);
 
   return (
     <div className="App">
-      <h2 className="pomodoro-title">Snoqualmie Pass Snowfall</h2>
-      <Timer workDuration={workDuration} breakDuration={breakDuration} />
-      <Settings
-        onSettingsChange={(newWorkDuration, newBreakDuration) => {
-          setWorkDuration(newWorkDuration);
-          setBreakDuration(newBreakDuration);
-        }}
-      />
-      <Sound />
+      <h2 className="project-title">Snoqualmie Pass Snowfall</h2>
+      <div className="container">
+        <img src={logo} alt="Logo" className="logo" />
+      </div>
     </div>
   );
 }
